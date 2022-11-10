6ec99b68e958d60af5c092ccd118835c526cf306
JuanJoseRodriguezF <115745541+JuanJoseRodriguezF@users.noreply.github.com>
2022-11-10 18:20:05 -0500

    Add files via upload
    Adjunto quiz

---
+++ Quiz diccionarios.py
@@ -0,0 +1,13 @@
+rios = {'Amazonas': 'Colombia', 'Yang Tse Kiang': 'China', 'Liena': 'Siberia', 'Niger': 'Nigeria', 'Mackenzie': 'USA'}
+for a in rios:
+    print("El rio "+a+' pasa por '+rios[a])
+print('---------------------')
+for b in rios.keys():
+    print(b)
+print('---------------------')
+for c in rios.values():
+    print(c)
+print('---------------------')
+palabras = {'programar': 'La accion de programacion', 'pensar': 'La habilidad de buscar una solución', 'logica': 'La habilidad de razonar', 'codigos': 'Los items de un programa', 'lenguajes': 'Distintos programas con distintas reglas para programar'}
+for a in palabras:
+    print(a+': '+palabras[a])
